060da772e7ce2e5fd3ef9b8c2ef431eabe8e0410
Prajas02 <zero20206@outlook.com>
2024-06-02 14:40:32 +0530

    Second Commit for the day

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()]
+});
+
+// tailwind cli and any other dependecies will be put here 
+// our whole code will be goiung in src
